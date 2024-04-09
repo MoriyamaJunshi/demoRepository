811c9e9e021511ce814da99cac4101271aa01949
moriyama junshi <junshi614@gmail.com>
2024-04-09 16:44:49 +0900

    練習3-4

--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -3,11 +3,13 @@ package test;
 public class HelloWorld {
 	public static void main(String[] args) {
 		int x = 3;
-		int y = 5;
+		int y = 2;
 		if(x > y) {
-			System.out.println(x);
+			System.out.println("xはyより大きい");
+		}else if(x < y) {
+			System.out.println("xはyより小さい");
 		}else {
-			System.out.println(y);
+			System.out.println("xとyは等しい");
 		}
 	}
 }

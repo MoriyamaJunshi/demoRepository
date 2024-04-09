9287f8faf55b5c3537914700c7b509172f90d672
moriyama junshi <junshi614@gmail.com>
2024-04-09 13:12:17 +0900

    練習問題1-2

--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -2,7 +2,8 @@ package test;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello, World");
+		int x = 11;
+		System.out.println("x = " + x);
 	}
 
 }

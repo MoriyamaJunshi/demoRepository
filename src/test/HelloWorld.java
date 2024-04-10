c0b0a7843ae0a9447142f7e8b96529f6a8bfcfae
moriyama junshi <junshi614@gmail.com>
2024-04-10 16:27:14 +0900

    練習3-6

--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -2,11 +2,21 @@ package test;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		int x = 5;
-		if(x % 2 == 0) {
-			System.out.println("xは偶数です");
+		int x = -1;
+		if (x > 0) {
+			  if (x % 2 == 0) {
+			    System.out.println("xは正の偶数です");
+			  } else {
+			    System.out.println("xは正の奇数です");
+			  }
+		} else if (x < 0) {
+			  if (x % 2 == 0) {
+			    System.out.println("xは負の偶数です");
+			  } else {
+			    System.out.println("xは負の奇数です");
+			  }
 		}else {
-			System.out.println("xは奇数です");
+			System.out.println("xは正の偶数です");
 		}
 	}
 }

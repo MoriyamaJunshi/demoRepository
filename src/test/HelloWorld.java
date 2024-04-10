aa0d9ff0c8117a56717d19fb6f392c1dada3398e
moriyama junshi <junshi614@gmail.com>
2024-04-10 16:41:58 +0900

    練習4-1

--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -2,15 +2,8 @@ package test;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		int x = 56;
-		if (x >= 80) {
-			System.out.println("優");
-		}else if(x >= 70) {
-			System.out.println("良");
-		}else if(x >= 60) {
-			System.out.println("可");
-		}else {
-			System.out.println("不可");
+		for(int i = 0; i < 10 ;i++ ) {
+			System.out.println("SPAM");
 		}
 	}
 }

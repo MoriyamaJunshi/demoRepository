0dda0c8a7968f97fca1a52e5e7bf9cad576e5743
moriyama junshi <junshi614@gmail.com>
2024-04-10 16:44:41 +0900

    練習4-2

--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -2,8 +2,8 @@ package test;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		for(int i = 0; i < 10 ;i++ ) {
-			System.out.println("SPAM");
+		for(int i = 3; i < 30; i += 3 ) {
+			System.out.println(i);
 		}
 	}
 }

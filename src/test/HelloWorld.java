117f6ea977591caf2f9b5f31c5fb36081f67c339
moriyama junshi <junshi614@gmail.com>
2024-04-10 17:23:49 +0900

    練習4-5

--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -1,11 +1,24 @@
 package test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class HelloWorld {
-	public static void main(String[] args) {
-		int x = 1;
-		for(int n = 1; n <= 8; n ++ ) {
-			x *= 2;
-			System.out.println("2の" + n + "乗は" + x);
-		}
-	}
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(
+                new InputStreamReader(System.in));
+        int sum = 0;
+
+        try {
+            for (int i = 0; i < 10; i++) {
+                int value = Integer.parseInt(br.readLine());
+                sum += value;
+            }
+            System.out.println("平均は" + (sum / 10));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+

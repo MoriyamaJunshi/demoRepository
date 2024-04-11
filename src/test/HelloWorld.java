61596b18c34cec8fdbdd88752ff758cf6251968d
moriyama junshi <junshi614@gmail.com>
2024-04-11 17:42:06 +0900

    練習1-12

--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -2,9 +2,11 @@ package test;
 
 public class HelloWorld {
     public static void main(String[] args) {
-    	int x = 14;
-    	System.out.println(x / 3);
-    	System.out.println(x % 3);
+    	int x = 5;
+    	x++;
+    	System.out.println(x);
+    	x--;
+    	System.out.println(x);
 
     }
 }

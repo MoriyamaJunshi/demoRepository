2fe5b44dfd9d9dcadec03a22a85430b18c02221f
moriyama junshi <junshi614@gmail.com>
2024-04-15 17:31:57 +0900

    練習2-2

--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -1,13 +1,16 @@
 package test;
 
-public class HelloWorld {
-    public static void main(String[] args) {
-    	int x = 5;
-    	x++;
-    	System.out.println(x);
-    	x--;
-    	System.out.println(x);
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class HelloWorld
+{
+    public static void main( String[] args ) throws IOException
+    {
+        BufferedReader br = new BufferedReader(
+                                new InputStreamReader( System.in ) );
+        int x = Integer.parseInt( br.readLine() );
+        System.out.println( x );
     }
 }
-

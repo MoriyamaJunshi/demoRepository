c5d17a191c0af2d586f88069d22a9e44eb7e36b0
moriyama junshi <junshi614@gmail.com>
2024-04-16 13:12:06 +0900

    練習3-9

--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -10,18 +10,30 @@ public class HelloWorld
     {
         BufferedReader br = new BufferedReader(
                                 new InputStreamReader( System.in ) );
-        System.out.println("中間試験点数を入力");
-        int midExam = Integer.parseInt( br.readLine() );
-        System.out.println("期末試験点数を入力");
-        int finExam = Integer.parseInt( br.readLine() );
-        if(midExam >= 60 && finExam >= 60) {
-        	System.out.println("合格");
-        }else if(midExam + finExam >= 130) {
-        	System.out.println("合格");
-        }else if(midExam + finExam >= 100 && (midExam>=90 || finExam>=90) ) {
-        	System.out.println("合格");
+        System.out.println("0=日曜、1=月曜、2=火曜、3=水曜、4=木曜、5=金曜、6=土曜");
+        int x = Integer.parseInt( br.readLine() );
+        System.out.println("0=午前、1=午後、2=夜間");
+        int y = Integer.parseInt( br.readLine() );
+        if(x == 1 || x == 4) {
+        	System.out.println("診療可能です。");
+        }else if(x == 2 || x == 5) {
+        	if(y == 0 ) {
+        		System.out.println("休診時間です。");
+        	}else {
+        		System.out.println("診療可能です。");
+        	}
+        }else if(x == 3 ) {
+        	if(y == 2 ) {
+        		System.out.println("休診時間です。");
+        	}else {
+        		System.out.println("診療可能です。");
+        	}
         }else {
-        	System.out.println("不合格");
+        	if(y == 0 ) {
+        		System.out.println("診療可能です。");
+        	}else {
+        		System.out.println("休診時間です。");
+        	}
         }
     }
 }

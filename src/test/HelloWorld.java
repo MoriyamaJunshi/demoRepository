9b4a97af5cc131679c60840315592134335f4bde
moriyama junshi <junshi614@gmail.com>
2024-04-16 14:52:03 +0900

    練習4-7

--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -6,36 +6,29 @@ import java.io.InputStreamReader;
 
 public class HelloWorld
 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(
                 new InputStreamReader(System.in));
-        int winCount = 0;
-        int loseCount = 0;
-        
-        try {
+        int giants = 0;
+        int tigers = 0;
 
-        for (int i = 0; i < 10; i++) {
-            System.out.print(i + 1 + "回目の対戦結果を入力 (0: 負け, 1: 勝ち): ");
-            int score = 	Integer.parseInt(br.readLine());
+        for( int game = 1 ; game <= 9 ; game++ )
+        {
+            System.out.print( game + "回表、巨人の得点は？" );
+            giants += Integer.parseInt( br.readLine() );
 
-            if (score == 0) {
-                loseCount++;
-            } else if (score == 1) {
-                winCount++;
-            } else {
-                System.out.println("不正な入力です。0 または 1 を入力してください。");
-                i--; // 入力ミスした場合はカウントしない
-            }
+            System.out.print( game + "回裏、阪神の得点は？" );
+            tigers += Integer.parseInt( br.readLine() );
         }
 
-        System.out.println("対戦結果:");
-        System.out.println("勝ち：" + winCount + "回");
-        System.out.println("負け：" + loseCount + "回");
-        
-        
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        System.out.println( "巨人：" + giants + "点, 阪神：" + tigers + "点" );
+
+        if( giants > tigers )
+            System.out.println( "巨人の勝ち" );
+        else if( giants < tigers )
+            System.out.println( "阪神の勝ち" );
+        else
+            System.out.println( "引き分け" );
     }
 }
 

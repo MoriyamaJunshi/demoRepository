1032bd4a91b2e9fa887047de3308e2849b258ca0
moriyama junshi <junshi614@gmail.com>
2024-04-16 10:34:49 +0900

    練習3-8

--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -10,7 +10,18 @@ public class HelloWorld
     {
         BufferedReader br = new BufferedReader(
                                 new InputStreamReader( System.in ) );
-        int x = Integer.parseInt( br.readLine() );
-        System.out.println(  x * 365 );
+        System.out.println("中間試験点数を入力");
+        int midExam = Integer.parseInt( br.readLine() );
+        System.out.println("期末試験点数を入力");
+        int finExam = Integer.parseInt( br.readLine() );
+        if(midExam >= 60 && finExam >= 60) {
+        	System.out.println("合格");
+        }else if(midExam + finExam >= 130) {
+        	System.out.println("合格");
+        }else if(midExam + finExam >= 100 && (midExam>=90 || finExam>=90) ) {
+        	System.out.println("合格");
+        }else {
+        	System.out.println("不合格");
+        }
     }
 }

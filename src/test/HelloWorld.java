3ed3df965e82390db67a212be28ccfa5698048dc
moriyama junshi <junshi614@gmail.com>
2024-04-16 10:11:18 +0900

    練習2-6

--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -11,7 +11,6 @@ public class HelloWorld
         BufferedReader br = new BufferedReader(
                                 new InputStreamReader( System.in ) );
         int x = Integer.parseInt( br.readLine() );
-        int y = Integer.parseInt( br.readLine() );
-        System.out.println( ( x + y )/2 );
+        System.out.println(  x * 365 );
     }
 }

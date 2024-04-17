0c16c5de0d434114f1db7c1667fa5696c92aa7ec
moriyama junshi <junshi614@gmail.com>
2024-04-17 16:50:40 +0900

    練習4-9

--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -10,16 +10,22 @@ public class HelloWorld
         BufferedReader br = new BufferedReader(
                 new InputStreamReader(System.in));
         int max_value = 0;
+        int min_value = 0;
 
         for( int i = 0 ; i < 10 ; i++ )
         {
             int value = Integer.parseInt( br.readLine() );
-
-            if( value > max_value )
-                max_value = value;
+            if(i==0) {
+            	max_value = value;
+            	min_value = value;
+            }else {
+                if( value > max_value )
+                    max_value = value;
+                if( value < min_value )
+                	min_value = value;         	
+            }
         }
-
-        System.out.println( "最大値=" + max_value );
+        System.out.println( "最大値=" + max_value + "最小値=" + min_value );
     }
 }
 

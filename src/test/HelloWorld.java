9a7ea3f6e5502be733aee15ddcfa0e43a9c9e6af
moriyama junshi <junshi614@gmail.com>
2024-04-21 20:28:37 +0900

    練習4-21

--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -12,10 +12,15 @@ public class HelloWorld{
 	        int size = Integer.parseInt( br.readLine() );
 	        for( int i = 0 ; i < size ; i++ )
 	        {
-	            for( int j = 0 ; j <= i ; j++ )
-	                System.out.print( "$" );
+	            for( int j = 0 ; j < size ; j++ )
+	            {
+	                if( i == j || ( size - i - 1 ) == j )
+	                    System.out.print( "X" );
+	                else
+	                    System.out.print( " " );
+	            }
 
-	            System.out.println();
+	            System.out.println( "" );
 	        }
 	}
 }
